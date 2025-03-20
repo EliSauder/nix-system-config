1db5054b1c9c5548585b56f9b45c372e685aea28
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-03-19 22:08:00 -0700

    fix(plat-spec): handle linux only settings & fix rev access

--- users/esauder.nix
+++ users/esauder.nix
@@ -1,7 +1,11 @@
-{pkg, ...}:
+{pkgs, options, lib, ...}:
 {
-  users.users.esauder = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "audio" ];
-  };
+    users.users.esauder = {
+    } //
+    lib.optionalAttrs (options?users.users.esauder.extraGroups) {
+        users.users.esauder.extraGroups = ["wheel" "audio"];
+    } //
+    lib.optionalAttrs (options?users.users.esauder.isNormalUser) {
+        users.users.esauder.isNormalUser = true;
+    };
 }
