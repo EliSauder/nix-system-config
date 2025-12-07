5244bb9f0a98ef1e32b6a12c221aa1f3c7b720f0
EliSauder <24995216+EliSauder@users.noreply.github.com>
2025-12-06 17:00:56 -0800

    fix: use correct -- for arges to Hyprland for start-hyprland

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -10,14 +10,14 @@ let
   hyprctlExe = "/run/current-system/sw/bin/hyprctl";
   cfg = config.greetd;
   hyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
-      exec-once = ${config.programs.regreet.package}/bin/regreet; ${hyprctlExe} dispatch exit
-    	misc {
-    	  disable_hyprland_logo = true
-    	  disable_splash_rendering = true
-        disable_hyprland_guiutils_check = true
-    	}
-    	env = GTK_USE_PORTAL,0
-    	env = GDK_DEBUG,no-portals
+    exec-once = ${config.programs.regreet.package}/bin/regreet; ${hyprctlExe} dispatch exit
+    misc {
+      disable_hyprland_logo = true
+      disable_splash_rendering = true
+      disable_hyprland_guiutils_check = true
+    }
+    env = GTK_USE_PORTAL,0
+    env = GDK_DEBUG,no-portals
   '';
 in
 {
@@ -29,7 +29,7 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${hyprlandExe} --config ${hyprlandConfig}";
+          command = "${hyprlandExe} -- --config ${hyprlandConfig}";
         };
       };
     };
