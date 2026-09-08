39d751547140264ff5de09a4f2674d7396a5059a
EliSauder <24995216+EliSauder@users.noreply.github.com>
2026-09-07 19:25:46 -0700

    feat: update dt-thiker-gear to new setup

--- hosts/dt-thinker-gear/configuration.nix
+++ hosts/dt-thinker-gear/configuration.nix
@@ -1,13 +1,19 @@
-{ config, lib, inputs, pkgs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
 
 {
   imports = [
-      ./hardware-configuration.nix
-      ./../../modules/graphics/amd.nix
-      ./../../modules/sharedconfigs.nix
-      ./../../users/esauder.nix
-      ./../../modules/ui.nix
-    ];
+    ./hardware-configuration.nix
+    ./../../modules/graphics/amd.nix
+    ./../../modules/sharedconfigs.nix
+    ./../../users/esauder.nix
+    ./../../modules/ui.nix
+  ];
 
   greetd.enable = true;
   hyprland.enable = true;
@@ -16,10 +22,27 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   networkingsvcs.enable = true;
-  networkingsvcs.hostName = "dt-thinker-gear";
+  networkingsvcs.hostName = "dt-captive-snack";
+  networkingsvcs.allowPing = true;
+
+  #boot.blacklistedKernelModules = [
+  #  "snd_seq_dummy"
+  #  "snd_seq_midi"
+  #  "snd_seq_midi_event"
+  #  "snd_seq_device"
+  #  "snd_seq"
+  #];
+
+  musnix = {
+    enable = true;
+    #rtcqs.enable = true;
+    #kernel.realtime = true;
+    #kernel.packages = pkgs.linuxPackages_rt;
+    #alsaSeq.enable = false;
+    #das_watchdog.enable = true;
+  };
 
   # Original NixInstalled Version (DO NOT CHANGE)
   system.stateVersion = "24.05";
 
 }
-
